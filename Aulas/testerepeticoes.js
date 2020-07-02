5f312302fef6223cce4d9f5b82ea0270e74e6bf9
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2020-07-02 15:45:48 -0300

    Exemplos

--- Aulas/testerepeticoes.js
+++ Aulas/testerepeticoes.js
@@ -1,6 +1,49 @@
-var casa = 2
- 
-while  (casa <=8) {
-    console.log ('porta aberta')
 
+/*
+
+----------------------------------------------------
+    LAÇO DE REPETIÇÃO COM VARIAVEL DE CONTROLE 
+------------------------------------------------------
+
+EXEMPLO 1
+
+for (var m= 3; m<=6; m++) {
+    console.log (`temos ${m} blocos`)
 }
+
+
+
+----------------------------------------------------
+    LAÇO DE REPETIÇÃO COM CONTROLE LOGICO NO FINAL
+------------------------------------------------------
+
+        EXEMPLO 1 
+    var lapis = 3
+    do {
+    console.log ( ` Lápis numero ${lapis}`)
+    lapis ++
+        } while ( lapis <=6)    
+ 
+
+
+ 
+ ----------------------------------------------------
+    LAÇO DE REPETIÇÃO COM CONTROLE LOGICO NO INICIO
+------------------------------------------------------
+
+   EXEMPLO 1 
+   
+     var casa = 2
+     while  (casa <=4) {
+    console.log ('porta aberta')
+     casa++                            // este ++ é referente ao contador para parar o laço. 
+                    }
+    
+    
+    EXEMPLO 2
+    var idade = 1
+    while (idade <=4) {
+    console.log (` Sua Idade é ${idade} anos`)
+    idade++                                // nunca esqueça de incrementar, pois do contrario vai bugar o sistema, vai rodar infinito)
+}
+     */
