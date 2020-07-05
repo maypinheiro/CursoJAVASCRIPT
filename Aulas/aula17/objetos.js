67f83587340449990feb20bf86a0f35578cef01d
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2020-07-05 15:52:18 -0300

    teste

--- Aulas/aula17/objetos.js
+++ Aulas/aula17/objetos.js
@@ -3,10 +3,11 @@ let amigo = {
     peso:'85',
     sexo:'M',
     engordar (p=0) {
-    this.peso += 
-    p
-}}
+        this.peso += p
+}
 
-amigo.engordar (1)
+}
+
+amigo.engordar ()
 console.log  (`${amigo.nome} pesa  ${amigo.peso} kg`)
 // verificar o pq esta adicionando o 1 no final.
